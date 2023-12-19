c9532c083e8f7e8a77a12c782e73bb51605c85c1
Maxuel Assis <maxuelassis95@gmail.com>
2023-12-19 17:07:33 -0300

    Correção da informação do número total de voltas completadas
    - Realizei a correção do bug que mostrava apenas o vencedor com as 4 voltas concluidas, o erro era na lógica de verificação de voltas.
    
    Falta implementar:
    
    - Tempo total de corrida

--- index.php
+++ index.php
@@ -9,9 +9,11 @@ fgets($arquivo);
 // Inicializa um array para armazenar os resultados da corrida
 $resultadosCorrida = [];
 
+// Inicializa um array para armazenar o número total de voltas de cada piloto
+$totalVoltas = [];
+
 // Lê o arquivo linha por linha
 while (!feof($arquivo)) {
-
     // Obtém a linha atual
     $linha = fgets($arquivo);
 
@@ -19,22 +21,18 @@ while (!feof($arquivo)) {
     $partes = explode(" ", $linha);
 
     // Extrai os dados necessários
-    $hora = $partes[0];
     $codigoPiloto = $partes[1];
     $numeroVolta = $partes[2];
     $tempoVolta = $partes[3];
-    $velocidadeMediaVolta = $partes[4];
-
-    // Procura o nome do piloto corretamente usando uma expressão regular
-    preg_match('/\d+ – (.+?)\s/', $linha, $matches);
-    // Recupera parte da expressão regular que está o nome
-    $nomePiloto = $matches[1];
-
-    // Obtém apenas o código do piloto
-    $codigoPiloto = substr($codigoPiloto, 0, 3);
 
     // Verifica se o piloto já está nos resultados da corrida
     if (!isset($resultadosCorrida[$codigoPiloto])) {
+        // Encotra o nome do piloto corretamente usando uma expressão regular
+        preg_match('/\d+ – (.+?)\s/', $linha, $matches);
+        $nomePiloto = $matches[1];
+
+        // Obtém apenas o código do piloto
+        $codigoPiloto = substr($codigoPiloto, 0, 3);
 
         // Inicializa os dados do piloto
         $resultadosCorrida[$codigoPiloto] = [
@@ -42,20 +40,23 @@ while (!feof($arquivo)) {
             'nomePiloto' => $nomePiloto,
             'numeroVoltasCompletadas' => 0,
             'tempoTotalCorrida' => 0,
-            'notas' => ''
         ];
+
+        // Inicializa o número total de voltas do piloto
+        $totalVoltas[$codigoPiloto] = 0;
     }
 
     // Atualiza os dados do piloto
     $resultadosCorrida[$codigoPiloto]['numeroVoltasCompletadas']++;
 
-    // Calcula o tempo total de corrida
-    if ($resultadosCorrida[$codigoPiloto]['numeroVoltasCompletadas'] <= 4) {
-        $resultadosCorrida[$codigoPiloto]['tempoTotalCorrida'] += floatval($tempoVolta);
-    }
+    // Atualiza o número total de voltas do piloto
+    $totalVoltas[$codigoPiloto] = max($totalVoltas[$codigoPiloto], $numeroVolta);
 
-    // Se o piloto completou 4 voltas, termine a corrida
-    if ($resultadosCorrida[$codigoPiloto]['numeroVoltasCompletadas'] == 4) {
+    // Calcula o tempo total de corrida
+    $resultadosCorrida[$codigoPiloto]['tempoTotalCorrida'] += floatval($tempoVolta);
+
+    // Se o piloto completou todas as voltas, termine a corrida
+    if ($totalVoltas[$codigoPiloto] == 4) {
         break;
     }
 }
@@ -85,4 +86,3 @@ foreach ($resultadosCorrida as $posicao => $piloto) {
     echo "<br><br>";
 }
 
-?>
